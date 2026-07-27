bf4a7e55a1aab9b5a4ca410b6d2c47b45dba8eb3
olgapuzanova0311-art <olgapuzanova0311@gmail.com>
2026-07-28 00:31:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /* Адрес сервера уведомлений на Railway.
    Вставь свой адрес между кавычками, без слэша на конце.
    Пример: https://ola-production.up.railway.app */
-window.OLA_API = "";
+window.OLA_API = "ola-bot-production.up.railway.app";
